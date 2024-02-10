459d83638edab00c59aacb34b5b6d74aaf0063dd
Eyad ahmed <Eyad932004@outloo.com>
2024-02-10 22:04:47 +0200

    add some options to button.story.tsx

--- packages/client/src/components/button/Button.tsx
+++ packages/client/src/components/button/Button.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import './button.scss';
+import React from "react";
+import "./button.scss";
 
 export enum ButtonType {
-  PRIMARY = 'primary',
-  SUCCESS = 'success',
-  DARK = 'dark',
-  INFO = 'info',
-  SECONDARY = 'secondary',
+  PRIMARY = "primary",
+  SUCCESS = "success",
+  DARK = "dark",
+  INFO = "info",
+  SECONDARY = "secondary",
 }
 
 interface ButtonProps {
   label: string;
   type?: ButtonType;
   backgroundColor?: string;
-  size?: 'small' | 'medium' | 'large';
+  size?: "small" | "medium" | "large";
   onClick?: () => void;
 }
 
 export const Button = ({
-  label,
+  label = "Button",
   type = ButtonType.PRIMARY,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   ...props
 }: ButtonProps) => {
   return (
     <button
-      className={['btn', `btn-${type}`, `btn-${size}`].join(' ')}
+      className={["btn", `btn-${type}`, `btn-${size}`].join(" ")}
       style={{ backgroundColor }}
       type="button"
       {...props}
--- packages/client/src/components/button/button.stories.tsx
+++ packages/client/src/components/button/button.stories.tsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import { Meta, StoryObj } from "@storybook/react";
 
-import { Meta, StoryObj } from '@storybook/react';
-
-import { Button, ButtonType } from './button';
+import { Button, ButtonType } from "./button";
 
 const meta: Meta<typeof Button> = {
   component: Button,
+  parameters: {
+    layout: "centered",
+  },
+
+  tags: ["autodocs"],
   argTypes: {
-    label: { control: 'text', defaultValue: 'Button'},
-    backgroundColor: { control: 'color' },
+    label: { control: "text", defaultValue: "Button" },
+    backgroundColor: { control: "color" },
     type: {
-      control: 'select',
+      control: "radio",
       options: ButtonType,
     },
     size: {
-      control: 'radio',
-      options: ['small', 'medium', 'large'],
+      control: "radio",
+      options: ["small", "medium", "large"],
     },
   },
 };
@@ -24,5 +27,7 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const _Button: Story = {
-  render: ({label, type, size}) => <Button label={label} type={type} size={size} />,
+  render: ({ label, type, size }) => (
+    <Button label={label} type={type} size={size} />
+  ),
 };
